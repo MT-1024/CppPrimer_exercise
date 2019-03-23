306976332fbe42df0e3520baa43361d30a3b39f2
MT-1024 <34984343+MT-1024@users.noreply.github.com>
2019-03-23 19:46:11 +0800

    Create exercise3_17.cpp
    从cin读入一组词并把它们存入一个vector对象，然后设法把所有词都改为大写形式。输出改变后的结果，每个词占一行。

---
+++ chapter03/exercise3_17.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::vector;
+using std::string;
+
+int main()
+{
+	string temp;
+	vector<string> v1;
+	while (cin >> temp)
+	{
+		v1.push_back(temp);
+	}
+	for (auto &str : v1)
+	{
+		for (auto &i : str)
+			i = toupper(i);
+	}
+	for (auto i : v1)
+		cout << i << endl;
+	/*for (vector<string>::size_type index = 0; index < v1.size(); index++)
+	{
+		cout << v1[index] << endl;
+	}*/
+	return 0;
+}
